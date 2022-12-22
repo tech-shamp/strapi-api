5652193a546b1fac5bb52e7e1dff0bde50e1679d
tech-shamp <shampytechnical@gmail.com>
2022-12-23 02:27:13 +0500

    first deployment

--- config/database.js
+++ config/database.js
@@ -2,13 +2,12 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST", "localhost"),
-      port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "jobly"),
-      user: env("DATABASE_USERNAME", "postgres"),
-      password: env("DATABASE_PASSWORD", "iboy"),
-      schema: env("DATABASE_SCHEMA", "public"), // Not required
+      host: env("PGHOST", "containers-us-west-159.railway.app"),
+      port: env.int("PGPORT", 5522),
+      database: env("PGDATABASE", "railway"),
+      user: env("PGUSER", "postgres"),
+      password: env("PGPASSWORD", "7ZbNSjqh2La1cI7QzxRY"),
+      ssl: env.bool(true),
     },
-    debug: false,
   },
 });
