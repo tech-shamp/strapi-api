9877ed1e91a709d73829fd6896d5a09eda8e1813
tech-shamp <shampytechnical@gmail.com>
2022-12-23 02:18:20 +0500

    first deployment

--- env/production/database.js
+++ env/production/database.js
@@ -2,19 +2,19 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      PGHOST: env("DATABASE_HOST", "containers-us-west-159.railway.app"),
-      PGPORT: env.int("DATABASE_PORT", 5522),
-      PGDATABASE: env("DATABASE_NAME", "railway"),
-      PGUSER: env("DATABASE_USERNAME", "postgres"),
-      PGPASSWORD: env("DATABASE_PASSWORD", "7ZbNSjqh2La1cI7QzxRY"),
-      DATABASE_URL: env(
-        "DATABASE_URL",
+      host: env("PGHOST", "containers-us-west-159.railway.app"),
+      port: env.int("PGPORT", 5522),
+      database: env("PGDATABASE", "railway"),
+      user: env("PGUSER", "postgres"),
+      password: env("PGPASSWORD", "7ZbNSjqh2La1cI7QzxRY"),
+      ssl: env.bool(true),
+    },
+  },
+});
+
+/* 
+  "DATABASE_URL",
         `postgresql://${{ PGUSER }}:${{ PGPASSWORD }}@${{ PGHOST }}:${{
           PGPORT,
         }}/${{ PGDATABASE }}`
-      ),
-      schema: env("DATABASE_SCHEMA", "public"), // Not required
-    },
-    debug: false,
-  },
-});
+*/
